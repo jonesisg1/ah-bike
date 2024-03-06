2af364f531b054ecf12f10116f7d4d51758f8aad
Ian Jones <jonesi_mail@yahoo.co.uk>
2024-03-06 20:09:39 +0000

    Add Vercel package for analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 import htmx from 'astro-htmx';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(),htmx()]
+  integrations: [tailwind(), htmx()],
+  output: 'static',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    maxDuration: 8,
+  }),
 });
